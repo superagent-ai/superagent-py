fd67796d75414037bcdb272c865cd26b6bfe6bcf
fern-api <115122769+fern-api[bot]@users.noreply.github.com>
2023-11-01 09:11:02 +0000

    Release 0.1.21

--- src/superagent/core/client_wrapper.py
+++ src/superagent/core/client_wrapper.py
@@ -14,7 +14,7 @@ class BaseClientWrapper:
         headers: typing.Dict[str, str] = {
             "X-Fern-Language": "Python",
             "X-Fern-SDK-Name": "superagent-py",
-            "X-Fern-SDK-Version": "v0.1.20",
+            "X-Fern-SDK-Version": "v0.1.21",
         }
         token = self._get_token()
         if token is not None:
